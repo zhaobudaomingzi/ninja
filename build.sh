5a6ddf21760ceb815b76651fe893ac2516188d18
gngpp <gngpp@iMac.local>
2024-02-06 22:17:53 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -22,29 +22,35 @@ root=$(pwd)
 [ ! -d uploads ] && mkdir uploads
 
 # Separate arrays for target architectures and Docker images
-target_architectures=("x86_64-unknown-linux-musl" "aarch64-unknown-linux-musl" "armv7-unknown-linux-musleabi" "armv7-unknown-linux-musleabihf" "arm-unknown-linux-musleabi" "arm-unknown-linux-musleabihf" "armv5te-unknown-linux-musleabi" "i686-unknown-linux-gnu" "i586-unknown-linux-gnu" "x86_64-pc-windows-msvc")
-docker_images=("ghcr.io/gngpp/rust-musl-cross:x86_64-musl" "ghcr.io/gngpp/rust-musl-cross:aarch64-musl" "ghcr.io/gngpp/rust-musl-cross:armv7-musleabi" "ghcr.io/gngpp/rust-musl-cross:armv7-musleabihf" "ghcr.io/gngpp/rust-musl-cross:arm-musleabi" "ghcr.io/gngpp/rust-musl-cross:arm-musleabihf" "ghcr.io/gngpp/rust-musl-cross:armv5te-musleabi" "ghcr.io/gngpp/rust-musl-cross:i686-musl" "ghcr.io/gngpp/rust-musl-cross:i586-musl" "ghcr.io/gngpp/cargo-xwin:latest")
+target_architectures=(
+    "x86_64-unknown-linux-musl"
+    "aarch64-unknown-linux-musl"
+    "armv7-unknown-linux-musleabi"
+    "armv7-unknown-linux-musleabihf"
+    "arm-unknown-linux-musleabi"
+    "arm-unknown-linux-musleabihf"
+    "armv5te-unknown-linux-musleabi"
+    "i686-unknown-linux-gnu"
+    "i586-unknown-linux-gnu"
+    "x86_64-pc-windows-msvc"
+)
 
-get_docker_image() {
-    local target_arch="$1"
-    local index
-    for ((index = 0; index < ${#target_architectures[@]}; ++index)); do
-        if [ "${target_architectures[index]}" == "$target_arch" ]; then
-            echo "${docker_images[index]}"
-            return 0
-        fi
-    done
-
-    echo "Architecture not found"
-    return 1
+pull_docker_image() {
+    image="ghcr.io/gngpp/rust-musl-cross:$1"
+    echo "Pulling $image"
+    docker pull $image
 }
 
 rmi_docker_image() {
-    echo "Removing $1"
-    docker rmi $1
+    image="ghcr.io/gngpp/rust-musl-cross:$1"
+    echo "Removing $image docker image"
+    if [ "$rmi" = "true" ]; then
+        docker rmi $image
+    fi
 }
 
 build_macos_target() {
+    echo "Building $1"
     cargo build --release --target $1 --features mimalloc
     sudo chmod -R 777 target
     cd target/$1/release
@@ -56,6 +62,8 @@ build_macos_target() {
 }
 
 build_linux_target() {
+    docker_image="ghcr.io/gngpp/rust-musl-cross:$1"
+
     features=""
     if [ "$1" = "armv5te-unknown-linux-musleabi" ] || [ "$1" = "arm-unknown-linux-musleabi" ] || [ "$1" = "arm-unknown-linux-musleabihf" ]; then
         features="--features rpmalloc"
@@ -67,8 +75,7 @@ build_linux_target() {
         fi
     fi
 
-    docker_image=$(get_docker_image "$1")
-
+    echo "Building $1"
     docker run --rm -t --user=$UID:$(id -g $USER) \
         -v $(pwd):/home/rust/src \
         -v $HOME/.cargo/registry:/root/.cargo/registry \
@@ -90,8 +97,9 @@ build_linux_target() {
 }
 
 build_windows_target() {
-    docker_image=$(get_docker_image "$1")
+    docker_image="ghcr.io/gngpp/rust-musl-cross:$1"
 
+    echo "Building $1"
     docker run --rm -t \
         -v $(pwd):/home/rust/src \
         -v $HOME/.cargo/registry:/usr/local/cargo/registry \
@@ -109,48 +117,40 @@ build_windows_target() {
 }
 
 if [ "$os" = "windows" ]; then
-    target_list=(x86_64-pc-windows-msvc)
-    for target in "${target_list[@]}"; do
-        echo "Building $target"
-
-        docker_image=$(get_docker_image "$target")
-
-        build_windows_target "$target"
-
-        if [ "$rmi" = "true" ]; then
-            rmi_docker_image "$docker_image"
-        fi
-    done
+    target="x86_64-pc-windows-msvc"
+    pull_docker_image "$target"
+    build_windows_target "$target"
+    rmi_docker_image "$target"
 fi
 
 if [ "$os" = "linux" ]; then
-    target_list=(x86_64-unknown-linux-musl aarch64-unknown-linux-musl armv7-unknown-linux-musleabi armv7-unknown-linux-musleabihf armv5te-unknown-linux-musleabi arm-unknown-linux-musleabi arm-unknown-linux-musleabihf i686-unknown-linux-gnu i586-unknown-linux-gnu)
+    target_list=(
+        "x86_64-unknown-linux-musl"
+        "aarch64-unknown-linux-musl"
+        "armv7-unknown-linux-musleabi"
+        "armv7-unknown-linux-musleabihf"
+        "armv5te-unknown-linux-musleabi"
+        "arm-unknown-linux-musleabi"
+        "arm-unknown-linux-musleabihf"
+        "i686-unknown-linux-gnu"
+        "i586-unknown-linux-gnu"
+    )
 
     for target in "${target_list[@]}"; do
-        echo "Building $target"
-
-        docker_image=$(get_docker_image "$target")
-
-        if [ "$target" = "x86_64-pc-windows-msvc" ]; then
-            build_windows_target "$target"
-        else
-            build_linux_target "$target"
-        fi
-
-        if [ "$rmi" = "true" ]; then
-            rmi_docker_image "$docker_image"
-        fi
+        pull_docker_image "$target"
+        build_linux_target "$target"
+        rmi_docker_image "$target"
     done
 fi
 
 if [ "$os" = "macos" ]; then
-    if ! which upx &>/dev/null; then
-        brew install upx
-    fi
-    rustup target add x86_64-apple-darwin aarch64-apple-darwin
-    target_list=(x86_64-apple-darwin aarch64-apple-darwin)
+    target_list=(
+        "x86_64-apple-darwin"
+        "aarch64-apple-darwin"
+    )
     for target in "${target_list[@]}"; do
-        echo "Building $target"
+        echo "Adding $target to the build queue"
+        rustup target add "$target"
         build_macos_target "$target"
     done
 fi
